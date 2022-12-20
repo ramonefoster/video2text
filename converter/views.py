5e9eb9d9542121d9d7a359a6f9b7d83d3cd3b1b4
Ramon <ramonefoster@gmail.com>
2022-12-20 09:49:04 -0300

    working v1.0

--- converter/views.py
+++ converter/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.views.generic import View
-import pytube as yt
+import pytube as youtube
 from io import BytesIO
 import sys
 from scipy.io.wavfile import write
 import speech_recognition as sr
 import numpy as np
-import ffmpeg
+import datetime
+from pydub import AudioSegment
 
 # Create your views here.
 class IndexView(View):
@@ -15,30 +16,36 @@ class IndexView(View):
 
     def post(self, request):
         url = request.POST['video_url']
-        video2audio(url)
-        return HttpResponseRedirect('/converter')
+        erro, txt = video2audio(url)
+        return render(request, 'converter/index.html', {'error': erro, 'text_result': txt})
 
 def video2audio(url):
     buffer = BytesIO()
     mem_audio = BytesIO() 
     r = sr.Recognizer()
+    erro = ''
+    # teste url = https://youtu.be/naIkpQ_cIt0
     try:
-        youtube = yt.YouTube(url)
-        youtube.streams.get_audio_only().stream_to_buffer(buffer)
+        yt = youtube.YouTube(url)
+        yt.streams.get_by_itag(251).stream_to_buffer(buffer)
+        print(yt.streams.get_audio_only().audio_codec)
 
         buffer.seek(0) 
-        dat = np.asarray(bytearray(buffer.read()), dtype=np.int32)
-        write(mem_audio, 16000, dat)
+        audio = AudioSegment.from_file(buffer)
+        audio.export(mem_audio, format="wav")
+
         with sr.AudioFile(mem_audio) as source:
             try:
                 print("Starting to get text")               
                 audio = r.record(source)                
-                print(source.DURATION)
+                print("Duração do audio: ", datetime.timedelta(seconds=source.DURATION))
                 txt = r.recognize_google(audio, language="en-US", show_all=True)
                 print(txt)
-            except Exception as e:
-                print("ERRO", e)        
+            except sr.UnknownValueError:
+                erro = "Google Recognition didn't undestand the audio."  
+            except:
+                erro = "An Error has occurred"
     except Exception as e:
-        print("err", e)
+        erro = "Invalid Youtube URL or video is corrupted."
     
-    return
+    return (erro, txt)
